31c70658eafdfc2d519986611de0811dc144a63f
Clément Le Biez <clement.lebiez@yousign.fr>
2018-11-11 19:18:51 +0100

    SEO 3

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,10 +8,23 @@ export default () => {
         <Helmet>
             <meta charSet="utf-8" />
             <title>Clément Le Biez, Développeur</title>
-            <meta name="Keywords" content="développeur, front, back, portfolio, clément, le biez" />
+            <meta name="Keywords" content="développeur, front, back, portfolio, clément, le biez, Caen, France" />
             <meta
                 name="description"
-                content="Je suis un développeur créatif front et back qui peut certainement répondre à votre besoin. N'hésitez pas à me contacter !" />
+                content="Je suis un développeur créatif front et back basé à Caen en France qui peut certainement répondre à votre besoin. N'hésitez pas à me contacter !" />
+            <script type="application/ld+json">
+                {
+                    "@context": "http://schema.org/",
+                    "@type": "Person",
+                    "name": "Clément Le Biez",
+                    "jobTitle": "Développeur front et back",
+                    "address": {
+                        "@type": "PostalAddress",
+                        "addressLocality": "Caen",
+                        "addressRegion": "France"
+                    }
+                }
+            </script>
         </Helmet>
         <FirstView />
         <UseCases />
