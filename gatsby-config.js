596c3b00fcc5cefa19ec7ce2bf0c63539b9afcd0
Clément Le Biez <clement.lebiez@yousign.fr>
2018-11-11 19:29:23 +0100

    add robots txt

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,13 @@
 module.exports = {
-    plugins: ['gatsby-plugin-react-helmet']
+    plugins: [
+      'gatsby-plugin-react-helmet',
+      {
+        resolve: 'gatsby-plugin-robots-txt',
+        options: {
+          host: 'https://www.clebiez.com',
+          sitemap: 'https://www.clebiez.com/sitemap.xml',
+          policy: [{ userAgent: '*', allow: '/' }]
+        }
+      }
+    ]
   }
