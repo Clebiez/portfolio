ef1d8c0cf693936a1fbbbdb0ff5b4186115b5236
Clément Le Biez <clement.lebiez@yousign.com>
2020-07-20 16:24:18 +0200

    Fix

--- src/components/useCaseItem.js
+++ src/components/useCaseItem.js
@@ -4,31 +4,44 @@ import theme from "../config/theme";
 
 const UseCaseItemWrapper = styled.div`
   position: relative;
-  height: 400px;
-  width: 600px;
+  height: 200px;
+  width: 300px;
+  border-radius: 4px;
   background-image: url(${(props) => props.picture});
-  background-size: 100%;
+  background-size: ${(props) =>
+    props.pictureWidth ? props.pictureWidth : "100%"};
   cursor: pointer;
+  margin: 1rem;
+  border: 1px solid ${theme.colors.darkBlue};
+  overflow: hidden;
 
   &:hover > div {
     transition: 0.5s all ease;
-    transform: translateY(0px);
+    transform: translateY(0);
     height: 100%;
+  }
+
+  &:hover p {
+    opacity: 1;
+    visibility: visible;
   }
 
   &:hover .underline {
     width: 100px;
+  }
+
+  @media (min-width: ${theme.breakpoints.tab}px) {
+    height: 275px;
+    width: 500px;
   }
 `;
 
 const UseCaseItemContent = styled.div`
   font-family: ${theme.fonts.title};
   color: ${theme.colors.darkBlue};
-  height: 100px;
-  padding: 1rem;
-  position: absolute;
-  bottom: 0;
-  width: 100%;
+  height: 75px;
+  padding: 0.5rem 1rem;
+  transform: translateY(100%);
   background-color: ${theme.colors.white};
   .underline {
     height: 2px;
@@ -36,13 +49,34 @@ const UseCaseItemContent = styled.div`
     width: 50px;
     transition: 0.3s all ease;
   }
+
+  @media (min-width: ${theme.breakpoints.tab}px) {
+    transform: translateY(275px) translateY(-75px);
+  }
 `;
 
-export default ({name, subTitle, picture}) => (
+const UseCaseItemTitle = styled.h3`
+
+`;
+
+const UseCaseItemDescription = styled.p`
+  opacity: 0;
+
+  visibility: hidden;
+  transition: 0.3s opacity ease;
+`;
+
+const UseCaseItemButton = styled.a`
+  cursor: pointer;
+`;
+
+export default ({name, description, picture}) => (
   <UseCaseItemWrapper picture={picture}>
     <UseCaseItemContent>
-      <h3>{name}</h3>
+      <UseCaseItemTitle>{name}</UseCaseItemTitle>
       <div className="underline"></div>
+      <UseCaseItemDescription>{description}</UseCaseItemDescription>
+      {/* <UseCaseItemButton>Je t'en dis plus ici</UseCaseItemButton> */}
     </UseCaseItemContent>
   </UseCaseItemWrapper>
 );
--- src/components/useCases.js
+++ src/components/useCases.js
@@ -1,14 +1,15 @@
-import React, {Component} from "react";
+import React from "react";
 import styled from "styled-components";
 import theme from "../config/theme";
 import SubTitle from "./subTitle";
-import UseCasesData from "../data/useCases.js";
 import UseCaseItem from "./useCaseItem";
 
 import HoheyThumb from "../static/hohey_thumb.png";
 import P2MYThumb from "../static/p2my_thumb.jpg";
 import YousignThumb from "../static/yousign_thumb.png";
 import EquilearningThumb from "../static/equilearning_thumb.jpg";
+import TeachingThumb from "../static/teaching_thumb.jpg";
+import ProjectCompleted from "../static/project_completed.svg";
 
 const UseCasesWrapper = styled.div`
   background-color: ${theme.colors.white};
@@ -30,12 +31,37 @@ const UseCasesComponent = () => (
     <SubTitle>Quelques unes de mes réalisations</SubTitle>
 
     <UseCasesList>
-      <UseCaseItem picture={P2MYThumb} name="Play2MeetYou" />
-      <UseCaseItem picture={YousignThumb} name="Yousign" />
-      <UseCaseItem picture={EquilearningThumb} name="EquiLearning" />
-      <UseCaseItem picture={HoheyThumb} name="Hohey" />
-      <UseCaseItem picture={HoheyThumb} name="Enseignements" />
-      <UseCaseItem picture={HoheyThumb} name="Enseignements" />
+      <UseCaseItem
+        picture={P2MYThumb}
+        name="Play2MeetYou"
+        description="Application mobile visant à connecter les utilisateurs entre eux via divers mini-jeux."
+      />
+      <UseCaseItem
+        picture={YousignThumb}
+        name="Yousign"
+        description="Application SaaS de signature électronique certifié permettant de simplifier les processus de signature dans les entreprises."
+      />
+      <UseCaseItem
+        picture={EquilearningThumb}
+        name="EquiLearning"
+        description="Application mobile de lexique Anglais - Français dans le domaine équin."
+      />
+      <UseCaseItem
+        picture={HoheyThumb}
+        name="HoHey"
+        description="Application web de comparateur de station de skis simple et efficace."
+      />
+      <UseCaseItem
+        picture={TeachingThumb}
+        name="Enseignements"
+        description="Enseignement de conduite de projet agile au seins de l'Université de Caen Normandie."
+      />
+      <UseCaseItem
+        picture={ProjectCompleted}
+        pictureWidth="50%"
+        name="Et d'autres encore !"
+        description="Je ne vais pas tout vous dire non plus :), n'hésitez pas à me contacter si vous voulez en savoir plus."
+      />
     </UseCasesList>
   </UseCasesWrapper>
 );
