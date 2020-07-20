a40747d92051f3e18de46a10386c2f8a7640e9be
Clément Le Biez <clement.lebiez@yousign.com>
2020-07-20 16:55:19 +0200

    Fix helmet & remove portfolio

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,24 +1,28 @@
 import React from "react";
-import { Helmet } from 'react-helmet';
-import FirstView from '../components/firstView';
-import Resume from '../components/resume';
-import UseCases from '../components/useCases';
-import SocialBar from '../components/socialBar';
+import {Helmet} from "react-helmet";
+import FirstView from "../components/firstView";
+import Resume from "../components/resume";
+import UseCases from "../components/useCases";
+import SocialBar from "../components/socialBar";
 
 export default () => {
-    return <>
-        <Helmet>
-            <meta charSet="utf-8" />
-            <title>Clément Le Biez, Développeur Front et Back à Caen</title>
-            <meta
-                name="description"
-                content="Je suis un développeur créatif front et back basé à Caen en France qui peut certainement répondre à votre besoin. N'hésitez pas à me contacter !" />
-            <script type="application/ld+json">{`
+  return (
+    <>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>
+          Clément Le Biez, Développeur web & mobile
+        </title>
+        <meta
+          name="description"
+          content="Je suis un développeur web & mobile spécialisé dans la gestion de projet agile. Je peux certainement répondre à votre besoin alors n'hésitez pas à me contacter !"
+        />
+        <script type="application/ld+json">{`
                 {
                     "@context": "http://schema.org/",
                     "@type": "Person",
                     "name": "Clément Le Biez",
-                    "jobTitle": "Développeur front et back",
+                    "jobTitle": "Développeur web & mobile",
                     "address": {
                         "@type": "PostalAddress",
                         "addressLocality": "Caen",
@@ -26,10 +30,11 @@ export default () => {
                     }
                 }
             `}</script>
-        </Helmet>
-        <FirstView />
-        <Resume />
-        <UseCases />
-        <SocialBar />
-    </>;
-}
+      </Helmet>
+      <FirstView />
+      <Resume />
+      <UseCases />
+      <SocialBar />
+    </>
+  );
+};
